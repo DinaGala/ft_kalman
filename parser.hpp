f9765d838863a9feba0e417b017bcef49df32800
Nadezhda Zhuzhleva <nzhuzhle@car2s4.42barcelona.com>
2025-11-11 21:50:43 +0100

    fixed some parsing

--- parser.hpp
+++ parser.hpp
@@ -13,3 +13,7 @@ struct Vec3 {
 // Parse whitespace separated doubles from a message. Returns optional Vec3 if
 // at least three doubles available. Additional values are ignored.
 std::optional<Vec3> parseVec3(const std::string &msg);
+
+// Parse a single double value from a message. Returns optional<double> if a
+// numeric token is found.
+std::optional<double> parseDouble(const std::string &msg);
